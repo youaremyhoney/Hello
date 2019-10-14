7148fda5f6f6bc47b9cfdb5d16f67c3e2a6c1b14
刘自强 <525409941@qq.com>
2019-10-14 18:19:12 +0800

    线程池改造的3个人卖30张票

---
+++ src/main/java/ReadWriteLockDemo.java
@@ -0,0 +1,23 @@
+import javax.swing.*;
+import java.net.SocketTimeoutException;
+
+class ReadWrite
+{
+    void read()
+    {
+
+    }
+
+    void write()
+    {
+
+    }
+}
+
+public class ReadWriteLockDemo {
+    public static void main(String[] args) {
+
+
+
+    }
+}
