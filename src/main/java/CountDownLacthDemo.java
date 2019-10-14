85c9b1b7254ec09513916f5e78aeac5806d50498
刘自强 <525409941@qq.com>
2019-10-14 20:15:58 +0800

    倒计时锁存器--当countDownLatch执行的次数达到你创建CountDownLatch的构造器里面的参数的时候，会执行countDownLatch.await()等待的方法

--- src/main/java/CountDownLacthDemo.java
+++ src/main/java/CountDownLacthDemo.java
@@ -1,17 +1,26 @@
+import java.sql.Time;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 public class CountDownLacthDemo {
     public static void main(String[] args) throws  Exception {
         CountDownLatch  countDownLatch = new CountDownLatch(5);//创建一个倒计时锁存器 从0开始计数，每次countDownLatch一次就加1
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 5; i++)
+        {
             int finalI = i;
             new Thread(()->{
+                try {
+                    Thread.sleep(4000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
                 countDownLatch.countDown();//为倒计时加一
                 System.out.println(Thread.currentThread().getName() + "\t离开教室" + finalI);
             }).start();
         }
 
-        countDownLatch.await();//挂起当前线程，等countDownLatch构造方法里面的值计数到了以后执行
+        //countDownLatch.await();//挂起当前线程，等countDownLatch构造方法里面的值计数到了以后执行
+        countDownLatch.await(2, TimeUnit.SECONDS);
         System.out.println(Thread.currentThread().getName() + "\t离开教室");
     }
 }
